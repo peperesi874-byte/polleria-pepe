0f784489782ff6ec121789181b4ca4c5efe2b406
Pepe <peperesi874@gmail.com>
2025-10-02 22:07:09 -0600

    Agregado vista inicio y ruta en web.php

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pollería Pepe</title>
+</head>
+<body>
+    <h1>Bienvenido al sistema de Pollería Pepe 🐔</h1>
+</body>
+</html>
